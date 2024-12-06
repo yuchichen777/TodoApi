b0be159df9375fbd7339b7ecc1cc48dd0a7499e6
yuchichen777 <yuchichen777@gmail.com>
2024-12-06 21:41:53 +0800

    add apikey

---
+++ Authentication/ApiKeyMiddleware.cs
@@ -0,0 +1,36 @@
+﻿namespace TodoApi.Authentication
+{
+     public class ApiKeyMiddleware
+     {
+          private readonly RequestDelegate _next;
+          private const string APIKEY = "X-API-KEY";
+
+          public ApiKeyMiddleware(RequestDelegate next)
+          {
+               _next = next;
+          }
+
+          public async Task InvokeAsync(HttpContext context)
+          {
+               if (!context.Request.Headers.TryGetValue(APIKEY, out var extractedApiKey))
+               {
+                    context.Response.StatusCode = 401; // Unauthorized
+                    await context.Response.WriteAsync("API Key was not provided.");
+                    return;
+               }
+
+               var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+
+               var apiKey = appSettings.GetValue<string>(APIKEY);
+
+               if (!apiKey.Equals(extractedApiKey))
+               {
+                    context.Response.StatusCode = 401; // Unauthorized
+                    await context.Response.WriteAsync("Unauthorized client.");
+                    return;
+               }
+
+               await _next(context);
+          }
+     }
+}
--- Program.cs
+++ Program.cs
@@ -1,46 +1,48 @@
 
 using Microsoft.EntityFrameworkCore;
+using TodoApi.Authentication;
 using TodoApi.Models;
 using TodoApi.Repository;
 using TodoApi.Services;
 
 namespace TodoApi
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+     public class Program
+     {
+          public static void Main(string[] args)
+          {
+               var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+               // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+               builder.Services.AddControllers();
+               // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+               builder.Services.AddEndpointsApiExplorer();
+               builder.Services.AddSwaggerGen();
 
-            builder.Services.AddDbContext<DbContext, TodoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext")));
+               builder.Services.AddDbContext<DbContext, TodoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext")));
 
-            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-            builder.Services.AddScoped<ITodoService, TodoService>();
+               builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+               builder.Services.AddScoped<ITodoService, TodoService>();
 
-            var app = builder.Build();
+               var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+               // Configure the HTTP request pipeline.
+               if (app.Environment.IsDevelopment())
+               {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+               }
 
-            app.UseHttpsRedirection();
+               app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+               app.UseAuthorization();
 
+               app.UseMiddleware<ApiKeyMiddleware>();
 
-            app.MapControllers();
+               app.MapControllers();
 
-            app.Run();
-        }
-    }
+               app.Run();
+          }
+     }
 }
